76d4205ee382818c1bd0c5a3bb84d738f5278751
Jiri Kaplan <jiri.kaplan@tieto.com>
2021-01-25 16:03:39 +0100

    Introducing com.eclipsesource.megit.plugin to have more control.
    Should fix issue #8.
    Should implement things mentioned in #2.

---
+++ com.eclipsesource.megit.plugin/src/com/eclipsesource/megit/plugin/LifeCycle.java
@@ -0,0 +1,40 @@
+package com.eclipsesource.megit.plugin;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.e4.ui.workbench.IPresentationEngine;
+import org.eclipse.e4.ui.workbench.UIEvents;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.osgi.service.event.Event;
+
+public class LifeCycle {
+
+	private static final String FIRST_RUN_TAG = "firstRunTag";
+	private static final String EDITOR_AREA = "org.eclipse.ui.editorss";
+
+	@Optional
+	@Inject
+	public void minimizeEditorAreaOnAppStartup(
+			@UIEventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) final Event event, EModelService modelService,
+			MApplication app) {
+		MUIElement editorArea = modelService.find(EDITOR_AREA, app);
+		if (shouldMinimize(editorArea)) {
+			editorArea.getTags().add(IPresentationEngine.MINIMIZED);
+			editorArea.getTags().add(FIRST_RUN_TAG);
+		}
+	}
+
+	private boolean shouldMinimize(MUIElement element) {
+		if (element != null) {
+			List<String> tags = element.getTags();
+			return !tags.contains(FIRST_RUN_TAG) && !tags.contains(IPresentationEngine.MINIMIZED);
+		}
+		return false;
+	}
+}
